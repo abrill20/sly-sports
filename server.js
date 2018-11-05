ce296c975914cf84a0ca3c3ae0ce72bdf4cc2234
Aaron Brill <abrill@skidmore.edu>
2018-11-05 00:07:37 -0500

    last commit worked but trying to fix sub-routes

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use('/api/admin', admin);
 
 // maybe edit
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/sly-sports/index.html'))
+  res.sendFile(path.join(__dirname, '/src/index.html'))
 })
 
 app.listen(httpPort, () => {

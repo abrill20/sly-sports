d24585f2b9e9936a5b9c240cc5735ed0d6b47976
Aaron Brill <abrill@skidmore.edu>
2018-11-08 13:20:09 -0500

    fixing

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -16,6 +16,7 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.authService.getProfile().subscribe((profile:any) => {
       this.user = profile.user;
+      console.log(this.user);
     },
     err => {
       console.log(err);

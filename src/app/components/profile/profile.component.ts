f258eb870857db23dbf911790000e86273841c1f
Aaron Brill <abrill@skidmore.edu>
2018-11-08 13:47:15 -0500

    fixing

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -9,23 +9,37 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
-  user: Object;
+  user: any;
 
   constructor(private authService: AuthService, private router: Router) { }
 
-  async ngOnInit() {
-    let prof = await this.authService.getProfile()
-    console.log(prof);
-    prof.subscribe((profile:any) => {
-      this.user = profile.user;
-      console.log(this.user);
+  ngOnInit() {
+    // let prof = await this.authService.getProfile()
+    // console.log(prof);
+    // prof.subscribe((profile:any) => {
+    //   this.user = profile.user;
+    //   console.log(this.user);
+    // },
+    // err => {
+    //   console.log(err);
+    //   return false;
+    // })
+    // console.log(this.user);
+    this.getSomething().subscribe(result => {
+      this.user = result;
+      console.log("user is ", this.user)
+    })
+  }
+
+  getSomething() {
+    return this.authService.getProfile().map(result => {
+      this.user = result
+      console.log("getsome user is ", this.user);
     },
     err => {
       console.log(err);
       return false;
     })
-    console.log(this.user);
-
   }
 
 }

e362eb18138d4acfea67853a5274de9dbff62ef4
Aaron Brill <abrill@skidmore.edu>
2018-11-09 12:51:00 -0500

    first contact test

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MailService {
 
-  constructor(private http: HttpClient, private headers: HttpHeaders) { }
+  constructor(private http: HttpClient) { }
 
   sendEmail() {
     return this.http.post('incoming_mail', {user: {
